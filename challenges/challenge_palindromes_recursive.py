83950ce3cf4d9480cdee0cc49773e35af95475c1
Vitor Wagner <vitormwagner@gmail.com>
2023-06-01 14:10:56 -0300

    Style: :ok_hand: Lint fixes

--- challenges/challenge_palindromes_recursive.py
+++ challenges/challenge_palindromes_recursive.py
@@ -1,5 +1,8 @@
 def is_palindrome_recursive(word, low_index, high_index):
-    if len(word) == 0: return False
-    if word[low_index] != word[high_index]: return False
-    if high_index - low_index <= 1: return True
+    if len(word) == 0:
+        return False
+    if word[low_index] != word[high_index]:
+        return False
+    if high_index - low_index <= 1:
+        return True
     return is_palindrome_recursive(word, low_index + 1, high_index - 1)
