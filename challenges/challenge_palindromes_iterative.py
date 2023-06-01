976cb6975a0577f09f28aa2ce04f6ed4c4934d69
Vitor Wagner <vitormwagner@gmail.com>
2023-06-01 15:13:57 -0300

    feat: :sparkles: added iterative palindrome algorithm

--- challenges/challenge_palindromes_iterative.py
+++ challenges/challenge_palindromes_iterative.py
@@ -1,3 +1,5 @@
 def is_palindrome_iterative(word):
-    """Faça o código aqui."""
-    raise NotImplementedError
+    if len(word) == 0:
+        return False
+    return word == word[::-1]
+# https://www.w3schools.com/python/python_howto_reverse_string.asp
