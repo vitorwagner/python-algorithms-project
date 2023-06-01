03f0c445274f7281b7178ed6c46f248d08d2f691
Vitor Wagner <vitormwagner@gmail.com>
2023-06-01 14:06:39 -0300

    Feat: :sparkles: Adds recursive palindrome algorithm

--- challenges/challenge_palindromes_recursive.py
+++ challenges/challenge_palindromes_recursive.py
@@ -1,3 +1,5 @@
 def is_palindrome_recursive(word, low_index, high_index):
-    """Faça o código aqui."""
-    raise NotImplementedError
+    if len(word) == 0: return False
+    if word[low_index] != word[high_index]: return False
+    if high_index - low_index <= 1: return True
+    return is_palindrome_recursive(word, low_index + 1, high_index - 1)
