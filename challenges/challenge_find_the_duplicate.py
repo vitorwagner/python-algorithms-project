dfb38bacfdb8307dbb20927e920f4dfdd4b67693
Vitor Wagner <vitormwagner@gmail.com>
2023-06-01 16:09:22 -0300

    In progress: :construction: Partial implementation of find duplicate algorithm

--- challenges/challenge_find_the_duplicate.py
+++ challenges/challenge_find_the_duplicate.py
@@ -1,3 +1,15 @@
 def find_duplicate(nums):
-    """Faça o código aqui."""
-    raise NotImplementedError
+    if type(nums) != list or len(nums) == 0:
+        return False
+
+    newNums = list()
+
+    for i in nums:
+        if type(i) != int or i < 0:
+            return False
+        if i not in newNums:
+            newNums.append(i)
+        else:
+            return i
+    return False
+# https://www.trainingint.com/how-to-find-duplicates-in-a-python-list.html
