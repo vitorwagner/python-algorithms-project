d25f13feb10e515a9e68b3a53598e0a80a58f5e6
Vitor Wagner <vitormwagner@gmail.com>
2023-06-01 16:17:50 -0300

    feat: :sparkles: Added tests for encrypt message algorithm

--- tests/encrypt/test_encrypt.py
+++ tests/encrypt/test_encrypt.py
@@ -1,5 +1,31 @@
-# from challenges.challenge_encrypt_message import encrypt_message
+from challenges.challenge_encrypt_message import encrypt_message
+import pytest
 
 
 def test_encrypt_message():
-    pass
+    result = encrypt_message("Test", 1)
+    assert result == "T_tse"
+
+    result = encrypt_message("Test", 2)
+    assert result == "ts_eT"
+
+    result = encrypt_message("Test", 3)
+    assert result == "seT_t"
+
+    result = encrypt_message("Test", 4)
+    assert result == "tseT"
+
+    result = encrypt_message("Test", 6)
+    assert result == "tseT"
+
+    result = encrypt_message("a lost colony on tau ceti iv", 7)
+    assert result == " tsol a_vi itec uat no ynoloc"
+
+    result = encrypt_message("", 7)
+    assert result == ""
+
+    with pytest.raises(TypeError, match="tipo inválido para key"):
+        encrypt_message("Teste", "Teste")
+
+    with pytest.raises(TypeError, match="tipo inválido para message"):
+        encrypt_message(7, 7)
