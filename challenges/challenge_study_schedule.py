14311a03270f095490503ad09508d872d92553e3
Vitor Wagner <vitormwagner@gmail.com>
2023-06-01 15:06:05 -0300

    feat: :sparkles: Added study schedule algorithm

--- challenges/challenge_study_schedule.py
+++ challenges/challenge_study_schedule.py
@@ -1,3 +1,11 @@
 def study_schedule(permanence_period, target_time):
-    """Faça o código aqui."""
-    raise NotImplementedError
+    count = 0
+    if type(target_time) != int:
+        return None
+    for i in range(len(permanence_period)):
+        if type(permanence_period[i][0]) != int\
+                or type(permanence_period[i][1]) != int:
+            return None
+        if permanence_period[i][0] <= target_time <= permanence_period[i][1]:
+            count += 1
+    return count
