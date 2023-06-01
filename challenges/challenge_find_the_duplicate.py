594e7dfe8db9c4ec9932cf0a712034774c511432
Vitor Wagner <vitormwagner@gmail.com>
2023-06-01 17:00:59 -0300

    Refactor: :recycle: Refactored find duplicate algorithm to fix quadratic complexity

--- challenges/challenge_find_the_duplicate.py
+++ challenges/challenge_find_the_duplicate.py
@@ -2,13 +2,13 @@ def find_duplicate(nums):
     if type(nums) != list or len(nums) == 0:
         return False
 
-    newNums = list()
+    newNums = set()
 
     for i in nums:
         if type(i) != int or i < 0:
             return False
         if i not in newNums:
-            newNums.append(i)
+            newNums.add(i)
         else:
             return i
     return False
