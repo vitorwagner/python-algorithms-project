6b185ac87c008abbb5247e3183463f5c62ef7aa7
Vitor Wagner <vitormwagner@gmail.com>
2023-06-01 15:29:31 -0300

    Refactor: :recycle: Refactored study schedule algorithm

--- challenges/challenge_study_schedule.py
+++ challenges/challenge_study_schedule.py
@@ -2,10 +2,10 @@ def study_schedule(permanence_period, target_time):
     count = 0
     if type(target_time) != int:
         return None
-    for i in range(len(permanence_period)):
-        if type(permanence_period[i][0]) != int\
-                or type(permanence_period[i][1]) != int:
+    for student in permanence_period:
+        if type(student[0]) != int\
+                or type(student[1]) != int:
             return None
-        if permanence_period[i][0] <= target_time <= permanence_period[i][1]:
+        if student[0] <= target_time <= student[1]:
             count += 1
     return count
